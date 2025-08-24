8fc9e3566385859e2cec6c1721b268e8697c2d53
Yaroslav <aroslavdudkov194@gmail.com>
2025-08-24 19:05:34 +0300

    Start
    GLWF and GLL libraries added. Empty window implemented. No Vulkan3D in repository, so it is not possible to compile it just from the GIt.

---
+++ prot_engine/lve_window.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <string>
+
+namespace lve
+{
+
+	class LveWindow
+	{
+		void initWindow();
+
+		const int width, heigth;
+
+		std::string windowName;
+		GLFWwindow *window;
+
+	public:
+		LveWindow(int w, int h, std::string name);
+		~LveWindow();
+
+		bool shouldClose()
+		{
+			return glfwWindowShouldClose(window);
+		};
+	};
+
+}
