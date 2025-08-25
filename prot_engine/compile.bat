3edc3a4e40c5708affb8dde4722f249f6ec8d0eb
Yaroslav <aroslavdudkov194@gmail.com>
2025-08-25 23:20:37 +0300

    Simple Shaders
    .vert and .frag shaders added

---
+++ prot_engine/compile.bat
@@ -0,0 +1,4 @@
+E:\VulkanSDK\Bin\glslc.exe shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
+E:\VulkanSDK\Bin\glslc.exe shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
+
+pause
--- prot_engine/first_app.hpp
+++ prot_engine/first_app.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "lve_window.hpp"
+#include "lve_pipeline.hpp"
+
 
 namespace lve 
 {
@@ -14,7 +16,6 @@ namespace lve
 
 	private:
 		LveWindow lveWindow{ WIDTH, HEIGHT, "Heya" };
-
-
+		LvePipeline lvePipeline{ "shaders/simple_shader.vert.spv","shaders/simple_shader.frag.spv" };
 	};
 }
