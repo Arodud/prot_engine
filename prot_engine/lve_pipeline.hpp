0ef18e39aec45e40c2901898bb22356aa3425015
Yaroslav <aroslavdudkov194@gmail.com>
2025-08-28 16:20:48 +0300

    Continue of Graphics Pipeline configuration
    Still need to configure render and pipeline layout in the future

--- prot_engine/lve_pipeline.hpp
+++ prot_engine/lve_pipeline.hpp
@@ -9,7 +9,18 @@ namespace lve
 {
 	struct PipelineConfigInfo 
 	{
-		
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
 	};
 	class LvePipeline
 	{
@@ -26,11 +37,11 @@ namespace lve
 
 	public:
 		LvePipeline(LveDevice &device, const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo configInfo);
-		~LvePipeline() {};
+		~LvePipeline();
 
 		LvePipeline(const LvePipeline&) = delete;
 		void operator=(const LvePipeline&) = delete;
 
-		static PipelineConfigInfo deafultPipelineConfigInfo(uint32_t width, uint32_t heigth);
+		static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t heigth);
 	};
 }
